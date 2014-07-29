e5e849e7663775a1a01cddb31ebdea280f90718e
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-07-29 14:28:56 +0300

    Add skeleton for Boggle solver

---
+++ 1.Boggle/Boggle.java
@@ -0,0 +1,21 @@
+interface BoggleSolver {
+    String[] solve(char[][] board);
+}
+
+class GeneratePossibleWordsSolver implements BoggleSolver {
+    public String[] solve(char[][] board) {
+        return null;
+    }
+}
+
+class FindWordsFromDictionarySolver implements BoggleSolver {
+    public String[] solve(char[][] board) {
+        return null;
+    }
+}
+
+class Boggle {
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+}
