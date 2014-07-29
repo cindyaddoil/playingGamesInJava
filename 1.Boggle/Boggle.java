16f32f8407f39b869609164ac1abc23370500eca
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-07-29 17:30:27 +0300

    Add FindWordsFromDictionarySolver implementation

--- 1.Boggle/Boggle.java
+++ 1.Boggle/Boggle.java
@@ -1,19 +1,118 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
 
 interface BoggleSolver {
-    String[] solve(Board board);
+    List<String> solve(Board board);
 }
 
 class GeneratePossibleWordsSolver implements BoggleSolver {
-    public String[] solve(Board board) {
+    public List<String> solve(Board board) {
         return null;
     }
 }
 
 class FindWordsFromDictionarySolver implements BoggleSolver {
-    public String[] solve(Board board) {
+    public List<String> solve(Board board) {
+        List<String> words = readDictionary();
+        List<String> foundWords = new ArrayList<String>();
+
+        for (String word : words) {
+            if (findWord(word, board)) {
+                foundWords.add(word);
+            }
+        }
+
+        return foundWords;
+    }
+
+    private boolean findWord(String word, Board board) {
+        List<Node> startingNodes = findStartingNodes(board, word);
+
+        for (Node startingNode : startingNodes) {
+            Set<Position> visited = new HashSet<Position>();
+
+            visited.add(startingNode.getPosition());
+
+            if (searchForWord(1, word, board, startingNode, visited)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private List<Node> findStartingNodes(Board board, String word) {
+        List<Node> startingNodes = new ArrayList<Node>();
+
+        for (Node node : board.getNodes()) {
+            if (node.getValue() == word.charAt(0)) {
+                startingNodes.add(node);
+            }
+        }
+
+        return startingNodes;
+    }
+
+    public boolean searchForWord(int index, String word, Board board, Node currentNode, Set<Position> visited) {
+        if (index == word.length()) return true;
+
+        List<Node> neighbours = new ArrayList<Node>();
+
+        for (Position neighbour : currentNode.getNeighbours()) {
+            neighbours.add(positionToNode(neighbour, board));
+        }
+
+        List<Node> candidates = new ArrayList<Node>();
+
+        for (Node candidate : neighbours) {
+            if (visited.contains(candidate.getPosition())) continue;
+            if (candidate.getValue() == word.charAt(index)) {
+                candidates.add(candidate);
+            }
+        }
+
+        for (Node node : candidates) {
+            visited.add(node.getPosition());
+            if (searchForWord(index + 1, word, board, node, visited)) return true;
+            visited.remove(node.getPosition());
+        }
+
+        return false;
+    }
+
+    private Node positionToNode(Position position, Board board) {
+        for (Node node : board.getNodes()) {
+            if (position.equals(node.getPosition())) return node;
+        }
+
+        assert (false); // should never happen
+
         return null;
+    }
+
+    private List<String> readDictionary() {
+        String word;
+        List<String> words = new ArrayList<String>();
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("dict.txt"));
+
+            while ((word = bufferedReader.readLine()) != null) {
+                words.add(word);
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return words;
     }
 }
 
@@ -59,6 +158,18 @@ class Node {
         this.position = position;
         this.neighbours = neighbours;
     }
+
+    public char getValue() {
+        return value;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public List<Position> getNeighbours() {
+        return neighbours;
+    }
 }
 
 class Board {
@@ -89,7 +200,7 @@ class Boggle {
         assert (dimension * dimension == input.length()); // TODO: requires correct input handling
 
         for (int i = 0, sz = input.length(); i < sz; i++) {
-            nodes.set(i, buildNode(input.charAt(i), new Position(i / dimension, i % dimension), dimension));
+            nodes.add(buildNode(input.charAt(i), new Position(i / dimension, i % dimension), dimension));
         }
 
         return new Board(nodes);
@@ -129,7 +240,12 @@ class Boggle {
     }
 
     public void solve() {
-        solver.solve(board);
+        for (String foundWord : solver.solve(board)) {
+            if (foundWord.length() < 3) continue;
+            System.out.print(foundWord + " ");
+        }
+
+        System.out.println();
     }
 
     public static void main(String[] args) {
