2d3cc8f844437aa7b14dc657a68cc2c90a224b81
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-07-30 12:38:32 +0300

    Add NPuzzle skeleton

---
+++ 2.N-Puzzle/NPuzzle.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+interface NPuzzleSolver {
+    public void solve();
+}
+
+class DFSSolver implements NPuzzleSolver {
+    public void solve() {
+    }
+}
+
+class BFSSolver implements NPuzzleSolver {
+    public void solve() {
+    }
+}
+
+class AStarSolver implements NPuzzleSolver {
+    public void solve() {
+    }
+}
+
+class IDAStarSolver implements NPuzzleSolver {
+    public void solve() {
+    }
+}
+
+class NPuzzle {
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+}
