fd507aa75b0204345c83ae3cf32e6ac8758e13bd
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-07-31 15:49:22 +0300

    Read board from file

--- 2.N-Puzzle/NPuzzle.java
+++ 2.N-Puzzle/NPuzzle.java
@@ -1,31 +1,114 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 interface NPuzzleSolver {
-    public void solve();
+    public void solve(Board board);
 }
 
-class DFSSolver implements NPuzzleSolver {
-    public void solve() {
+abstract class NPuzzleIsSolvable {
+    /*
+     * NPuzzle is solvable when
+     * >>> zeroRow + numberOfInversions is even <<<
+     * where zeroRow is the row number of empty tile (row index starts from 1)
+     * where numberOfInversions is the amount of elements Ai and Aj such that Ai > Aj (Ai /= 0, Aj /= 0)
+     */
+    protected boolean isSolvable(Board board) {
+        // TODO
+        return false;
     }
 }
 
-class BFSSolver implements NPuzzleSolver {
-    public void solve() {
+class DFSSolver extends NPuzzleIsSolvable implements NPuzzleSolver {
+    final static int MAX_DEPTH = 50;
+
+    public void solve(Board board) {
     }
 }
 
-class AStarSolver implements NPuzzleSolver {
-    public void solve() {
+class BFSSolver extends NPuzzleIsSolvable implements NPuzzleSolver {
+    public void solve(Board board) {
     }
 }
 
-class IDAStarSolver implements NPuzzleSolver {
-    public void solve() {
+class AStarSolver extends NPuzzleIsSolvable implements NPuzzleSolver {
+    public void solve(Board board) {
+    }
+}
+
+class IDAStarSolver extends NPuzzleIsSolvable implements NPuzzleSolver {
+    public void solve(Board board) {
+    }
+}
+
+class Board {
+    private final int dimension;
+    private final int[] values;
+
+    public Board(int dimension, int[] values) {
+        this.dimension = dimension;
+        this.values = values;
+    }
+
+    public int getDimension() {
+        return dimension;
     }
 }
 
 class NPuzzle {
+    private Board board;
+    private NPuzzleSolver solver;
+
+    public NPuzzle(String fileName) {
+        board = buildBoard(fileName);
+        solver = new DFSSolver();
+        //solver = new BFSSolver();
+        //solver = new AStarSolver();
+        //solver = new IDAStarSolver();
+    }
+
+    private Board buildBoard(String fileName) {
+        String input = "";
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(" ");
+            }
+
+            input = stringBuilder.toString();
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Integer dimension = 0;
+        Scanner scanner = new Scanner(input);
+
+        // TODO: error handling for incorrect input file
+        dimension = scanner.nextInt();
+        int[] values = new int[dimension * dimension];
+
+        for (int i = 0, sz = dimension * dimension; i < sz; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return new Board(dimension, values);
+    }
+
+    public void solve() {
+        solver.solve(board);
+    }
+
     public static void main(String[] args) {
-        System.out.println("HI");
+        assert(args[0] != null); // TODO: requires correct input handling
+        new NPuzzle(args[0]).solve();
     }
 }
