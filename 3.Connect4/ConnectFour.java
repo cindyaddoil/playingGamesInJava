21a2ffd7a17333a6a382a90c63f75c6c3bc383ab
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-08-05 15:06:07 +0300

    Add HumanPlayer implementation

--- 3.Connect4/ConnectFour.java
+++ 3.Connect4/ConnectFour.java
@@ -1,29 +1,69 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 interface Player {
-    public PlayerMove makeMove(Board board);
+    public PlayerMove makeMove(ConnectFourGame game);
 }
 
 class HumanPlayer implements Player {
-    public PlayerMove makeMove(Board board) {
-        return null;
+    private String input;
+    private final BufferedReader inputStream;
+
+    public HumanPlayer() {
+        inputStream = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public PlayerMove makeMove(ConnectFourGame game) {
+        PlayerMove playerMove = null;
+
+        while (true) {
+            System.out.print("Select column [1-" + game.getBoard().NUMBER_OF_COLUMNS + "]: ");
+
+            try {
+                input = inputStream.readLine();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Failed to read your move! Please try again.");
+                continue;
+            }
+
+            try {
+                int columnIndex = Integer.parseInt(input);
+                playerMove = new PlayerMove(this, columnIndex);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid column! Please try again.");
+                continue;
+            }
+
+            if (!game.isValidMove(playerMove)) {
+                System.out.println("Invalid move! Please try again.");
+                continue;
+            }
+
+            break;
+        }
+
+        return playerMove;
     }
 }
 
 class MonteCarloPlayer implements Player {
-    public PlayerMove makeMove(Board board) {
+    public PlayerMove makeMove(ConnectFourGame game) {
         return null;
     }
 }
 
 class MinMaxPlayer implements Player {
-    public PlayerMove makeMove(Board board) {
+    public PlayerMove makeMove(ConnectFourGame game) {
         return null;
     }
 }
 
 class MinMaxAlphaBetaPlayer implements Player {
-    public PlayerMove makeMove(Board board) {
+    public PlayerMove makeMove(ConnectFourGame game) {
         return null;
     }
 }
@@ -47,13 +87,30 @@ class PlayerMove {
 }
 
 class Board {
-    private final static int NUMBER_OF_ROWS = 6;
-    private final static int NUMBER_OF_COLUMNS = 7;
+    public final static int NUMBER_OF_ROWS = 6;
+    public final static int NUMBER_OF_COLUMNS = 7;
 }
 
-class ConnectFourState {
+class ConnectFourGame {
     private Board board;
     private List<PlayerMove> moves;
+
+    public ConnectFourGame() {
+        board = new Board();
+        moves = new ArrayList<PlayerMove>();
+    }
+
+    public ConnectFourGame(Board board, List<PlayerMove> moves) {
+        // TODO: deep copy
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public boolean isValidMove(PlayerMove playerMove) {
+        return true; // TODO: add actual validation
+    }
 }
 
 class ConnectFour {
