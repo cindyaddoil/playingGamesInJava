034ebe11b12870bdf3b8e1eaa29100de93bbc469
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-08-05 13:56:51 +0300

    Connect4 skeleton update

--- 3.Connect4/ConnectFour.java
+++ 3.Connect4/ConnectFour.java
@@ -1,3 +1,37 @@
+interface Player {
+    public PlayerMove makeMove(Board board);
+}
+
+class HumanPlayer implements Player {
+    public PlayerMove makeMove(Board board) {
+        return null;
+    }
+}
+
+class MonteCarloPlayer implements Player {
+    public PlayerMove makeMove(Board board) {
+        return null;
+    }
+}
+
+class MinMaxPlayer implements Player {
+    public PlayerMove makeMove(Board board) {
+        return null;
+    }
+}
+
+class MinMaxAlphaBetaPlayer implements Player {
+    public PlayerMove makeMove(Board board) {
+        return null;
+    }
+}
+
+class PlayerMove {
+}
+
+class Board {
+}
+
 class ConnectFour {
     public static void main(String[] args) {
         System.out.println("HI");
